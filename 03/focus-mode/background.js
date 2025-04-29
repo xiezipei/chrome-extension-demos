930138c8d2d2c45de51abcccb0594625397f7f3c
xiezipei <zipeixie@gmail.com>
2025-04-29 14:27:48 +0800

    feat: 添加示例 03

--- 03/focus-mode/background.js
+++ 03/focus-mode/background.js
@@ -9,24 +9,21 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
-    // Set the action badge to the next state
     await chrome.action.setBadgeText({
       tabId: tab.id,
       text: nextState,
     });
     if (nextState === 'ON') {
-      // Insert the CSS file when the user turns the extension on
+      // 插入样式
       await chrome.scripting.insertCSS({
         files: ['focus-mode.css'],
         target: { tabId: tab.id },
       });
     } else if (nextState === 'OFF') {
-      // Remove the CSS file when the user turns the extension off
+      // 移除样式
       await chrome.scripting.removeCSS({
         files: ['focus-mode.css'],
         target: { tabId: tab.id },
